6b76767389132378372d07f8281daab1e3d6dffe
Daniel <daniel_rincon5@hotmail.com>
2022-11-02 14:43:01 -0600

    Se añadieron varias funciones en api aritmetica

--- go.mod
+++ go.mod
@@ -3,9 +3,15 @@ module github.com/Pcorner0/calculadora-avanzada
 go 1.19
 
 require (
+	github.com/gin-gonic/gin v1.8.1
+	github.com/spf13/viper v1.13.0
+	gorm.io/driver/postgres v1.4.5
+	gorm.io/gorm v1.24.1-0.20221019064659-5dd2bb482755
+)
+
+require (
 	github.com/fsnotify/fsnotify v1.5.4 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
-	github.com/gin-gonic/gin v1.8.1 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/go-playground/validator/v10 v10.11.1 // indirect
@@ -34,7 +40,6 @@ require (
 	github.com/spf13/cast v1.5.0 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	github.com/spf13/viper v1.13.0 // indirect
 	github.com/subosito/gotenv v1.4.1 // indirect
 	github.com/ugorji/go/codec v1.2.7 // indirect
 	golang.org/x/crypto v0.1.0 // indirect
@@ -45,6 +50,4 @@ require (
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
-	gorm.io/driver/postgres v1.4.5 // indirect
-	gorm.io/gorm v1.24.1-0.20221019064659-5dd2bb482755 // indirect
 )
--- pkg/api/aritmetica/handler.go
+++ pkg/api/aritmetica/handler.go
@@ -6,7 +6,12 @@ import (
 )
 
 type Handler interface {
-	SumNumbers(ctx *gin.Context)
+	SumSubtNumbers(ctx *gin.Context)
+	DivideTwoNumbers(ctx *gin.Context)
+	MultiplyNumbers(ctx *gin.Context)
+	RootNumbers(ctx *gin.Context)
+	PowerNumbers(ctx *gin.Context)
+	SinNumbers(ctx *gin.Context)
 }
 
 type handler struct {
@@ -19,16 +24,90 @@ func NewHandler(service Service) Handler {
 	}
 }
 
-func (h handler) SumNumbers(ctx *gin.Context) {
+func (h handler) SumSubtNumbers(ctx *gin.Context) {
 	var req numbersRequest
 	if err := ctx.ShouldBindJSON(&req); err != nil {
 		ctx.JSON(http.StatusBadRequest, errorResponse(err))
 	}
 
-	total, err := h.Service.SumNumbers(req)
+	total, err := h.Service.SumSubtNumbers(req)
 	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
-		ctx.JSON(http.StatusInternalServerError, total)
+		return
+	}
+
+	ctx.JSON(http.StatusOK, total)
+}
+
+func (h handler) DivideTwoNumbers(ctx *gin.Context) {
+	var req divideRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+	}
+
+	total, err := h.Service.DivideTwoNumbers(req)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, total)
+}
+
+func (h handler) MultiplyNumbers(ctx *gin.Context) {
+	var req multiplyRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+	}
+
+	total, err := h.Service.MultiplyNumbers(req)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, total)
+}
+
+func (h handler) RootNumbers(ctx *gin.Context) {
+	var req RootRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+	}
+
+	total, err := h.Service.RootNumbers(req)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, total)
+}
+
+func (h handler) PowerNumbers(ctx *gin.Context) {
+	var req PowerRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+	}
+
+	total, err := h.Service.PowerNumbers(req)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, total)
+}
+
+func (h handler) SinNumbers(ctx *gin.Context) {
+	var req SinRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+	}
+
+	total, err := h.Service.SinNumbers(req)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
 		return
 	}
 
